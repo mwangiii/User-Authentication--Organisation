4e227a625ef3140c2fb66b6c655c6f0f63c08bb9
mwangiii <mwangi8kevin@gmail.com>
2024-07-08 17:46:54 +0300

    init.py fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import config 
+from config import config
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 
@@ -12,23 +12,22 @@ db_params = config()
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}/{db_params['database']}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '4f8b31dc8ee3437486e3424bcb2d6f0b'
-app.config['JWT_TOKEN_LOCATION'] = ['headers'] 
+app.config['JWT_TOKEN_LOCATION'] = ['headers']
 
-
-# Initialize SQLAlchemy
+# Initialize extensions
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 migrate = Migrate(app, db)
-# Import models
+
+# Import models (this should be done before calling db.create_all())
 from app import models
 
-# Create database tables if they do not exist
-with app.app_context():
-    db.create_all()
+# Create database tables using Flask-Migrate (recommended method)
+# This will handle migrations and ensure database schema changes are managed properly
+@migrate.init_app(app)
+def create_or_update_db():
+    with app.app_context():
+        db.create_all()
 
-# Import routes 
+# Import routes
 from app import routes
-
-#import test
-# from app import test
-
