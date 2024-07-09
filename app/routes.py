dd57cba4c31a629dfbef0d0f28847f50ff7247a2
mwangiii <mwangi8kevin@gmail.com>
2024-07-09 13:05:29 +0300

    fixing the data params

--- app/routes.py
+++ app/routes.py
@@ -122,7 +122,6 @@ def register_user():
             "error": str(e)
         }), 500
 
-
 # Logs in a user. When you log in, you can select an organisation to interact with
 @app.route("/auth/login", methods=['POST'])
 def login_user():
