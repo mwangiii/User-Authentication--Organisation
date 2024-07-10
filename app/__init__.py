75ada917a70c9c320f0245f0e3174e38c6abf23b
mwangiii <mwangi8kevin@gmail.com>
2024-07-10 11:25:44 +0300

    creating a new db

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
-
+import os
 # Initialize Flask application
 app = Flask(__name__)
 
 # Load database configuration from database.ini using config function
 db_params = config()
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:QJKZuKBpgoInspGTLMrgWxlkfrSiLXKu@monorail.proxy.rlwy.net:17210/railway"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:QJKZuKBpgoInspGTLMrgWxlkfrSiLXKu@monorail.proxy.rlwy.net:17210/railway"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+# connect the postgress to localhost db
+# app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}/{db_params['database']}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '4f8b31dc8ee3437486e3424bcb2d6f0b'
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
